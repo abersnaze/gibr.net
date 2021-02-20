5f8b893112a546c1f31a71bafbca44e5088de9e5
George Campbell <gscampbell@netflix.com>
2021-02-19 18:24:24 -0800

    adding lettermark and fixing the font.

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 
 
 <main>
-	<h1>Hello, I'm George</h1>
+	<h1><img src="/lettermark.svg" alt="gibr.net logo" id="logo"/> Hello, I'm George</h1>
 	<p>
 		I'm a software developer at Netflix. My most influential contribution to
 		software is my work on RxJava and asynchronous APIs.
@@ -22,7 +22,7 @@
 </main>
 
 <style>
-	@import url("inconsolata.woff");
+	@import url("/inconsolata.woff");
 
 	main {
 		color: #424242;
@@ -33,7 +33,7 @@
 	}
 
 	.external {
-		background-image: url(/assets/external.svg);
+		background-image: url(/external.svg);
 		background-position: center right;
 		background-repeat: no-repeat;
 		padding-right: 13px;
@@ -42,6 +42,11 @@
 	h1 {
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	img,#logo {
+		height: 1em;
+		vertical-align: text-bottom;
 	}
 
 	@media (min-width: 640px) {
